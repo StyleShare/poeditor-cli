4fe4840421aee93df0dbcc8639c9c564bcebcf17
Suyeol Jeon <devxoul@gmail.com>
2017-02-10 16:32:33 +0900

    Add version checking before every commands

--- lib/poeditor/commands/command.rb
+++ lib/poeditor/commands/command.rb
@@ -1,8 +1,12 @@
+require 'json'
+require 'net/http'
+
 module POEditor
   class Command
     # The entry point of the CLI application
     def self.run(argv)
       UI.enabled = true
+      self.check_update
       command = self.command_class(argv).new
       begin
         command.run(argv)
@@ -19,6 +23,23 @@ module POEditor
         VersionCommand
       else
         HelpCommand
+      end
+    end
+
+    def self.check_update
+      begin
+        uri = URI("https://api.github.com"\
+                  "/repos/StyleShare/poeditor-cli/releases/latest")
+        data = JSON(Net::HTTP.get(uri))
+        latest = data["tag_name"]
+        if Gem::Version.new(VERSION) < Gem::Version.new(latest)
+          UI.puts %{\
+poeditor-cli #{latest} is available. (You're using #{VERSION})
+To update: `$ gem install poeditor-cli`
+Changelog: https://github.com/StyleShare/poeditor-cli/releases\
+          }.green
+        end
+      rescue
       end
     end
   end
